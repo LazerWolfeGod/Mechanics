20330f0dae6999d1bc9db7427c62b85615dab92d
Christian Harris <74326592+LazerWolfeGod@users.noreply.github.com>
2024-02-16 13:08:19 +0000

    Balls now detect collisions
    it is quite broken, but the base functionality works

--- balls bouncing.py
+++ balls bouncing.py
@@ -1,5 +1,6 @@
 import pygame,math,random,time
-from UIpygame import PyUI as pyui
+import numpy as np
+import PyUI as pyui
 pygame.init()
 screenw = 1200
 screenh = 900
@@ -24,6 +25,15 @@ class Vector2:
         return ((self.x)**2+(self.y)**2)**0.5
     def angle(self):
         return math.atan2(self.y,self.x)
+    def matrix(self):
+        return np.array([[self.x],[self.y]])
+    def set_matrix(self,matrix):
+        self.x = matrix[0][0]
+        self.y = matrix[1][0]
+    def __str__(self):
+        return  f'<Vector:{self.x},{self.y}>'
+    def __repr__(self):
+        return  f'<Vector:{self.x},{self.y}>'
 
 def root(x):
     if x<0: return -((-x)**0.5)
@@ -43,8 +53,8 @@ class Ball:
         self.energy = 0
         self.r = r
         self.mass = math.pi*self.r**2
-        self.restitution = 0.6
-        self.friction = 0.6
+        self.restitution = 0.5#0.6
+        self.friction = 0#0.6
         
         
         self.col = [random.randint(0,255) for a in range(3)]
@@ -54,17 +64,17 @@ class Ball:
 
         self.prepos = Vector2(self.x,self.y)
         
-        self.velocity.y+=9.81*system.delta_time
+        self.velocity.y+=9.81/60
         self.x+=self.velocity.x*system.delta_time
         self.y+=self.velocity.y*system.delta_time
         self.energy = 0.5*self.mass*self.velocity.magnitude()**2+9.81*self.mass*(system.height-self.y-self.r)
         
-
+        self.check_ball_collisions(system)
         self.check_wall_collisions(system)
         
         self.deltaR = self.prepos-Vector2(self.x,self.y)
-        if self.deltaR.magnitude() < 0.00001:
-            self.velocity = Vector2(random.gauss(0,3),-random.random()*20)
+##        if self.deltaR.magnitude() < 0.00001:
+##            self.velocity = Vector2(random.gauss(0,3),-random.random()*20)
         
     def check_wall_collisions(self,system):
         e = self.restitution*system.wall_restitution
@@ -77,11 +87,26 @@ class Ball:
             self.solid_collide(system,math.pi/2,e,mu)
         if self.y-self.r<0:
             self.y = self.r
-            self.solid_collide(system,0,e,mu)
+            self.solid_collide(system,math.pi,e,mu)
         elif self.y+self.r>system.height:
             self.y = system.height-self.r
             self.solid_collide(system,0,e,mu)
-
+    def check_ball_collisions(self,system):
+        for oy in range(-1,2):
+            for ox in range(-1,2):
+                x = self.collision_grid_x+ox
+                y = self.collision_grid_y+oy
+                if x>=0 and y>=0 and x<len(system.collision_grid[0]) and y<len(system.collision_grid):
+                    for ball in system.collision_grid[y][x]:
+                        if self!=ball and self.check_collision(ball):
+                            self.ball_collide(system,ball)
+##                            print("hit")
+    
+    def check_collision(self,ball):
+        dis = ((self.x-ball.x)**2+(self.y-ball.y)**2)**0.5
+        if dis<self.r+ball.r:
+            return True
+        return False
 
     def solid_collide(self,system,theta,e,mu):
         # Bounce
@@ -93,14 +118,60 @@ class Ball:
 
         # Friction
 
-        x_impulse = self.mass*(prevx-self.velocity.x)
-        y_impulse = self.mass*(prevy-self.velocity.y)
+        x_impulse = min(self.mass*(prevx-self.velocity.x),self.mass*9.81)
+        y_impulse = min(self.mass*(prevy-self.velocity.y),self.mass*9.81)
 
-        x_friction_energy = max(min(mu*y_impulse*abs(self.velocity.x)*system.delta_time*60,0.5*self.mass*self.velocity.x**2),0)
-        y_friction_energy = max(min(mu*x_impulse*abs(self.velocity.y)*system.delta_time*60,0.5*self.mass*self.velocity.y**2),0)
+        x_friction_energy = max(min(mu*y_impulse*abs(self.velocity.x),0.5*self.mass*self.velocity.x**2),0)
+        y_friction_energy = max(min(mu*x_impulse*abs(self.velocity.y),0.5*self.mass*self.velocity.y**2),0)
         
         if self.velocity.x!=0: self.velocity.x = ((0.5*self.mass*self.velocity.x**2-x_friction_energy)*2/self.mass)**0.5*(self.velocity.x/abs(self.velocity.x))
         if self.velocity.y!=0: self.velocity.y = ((0.5*self.mass*self.velocity.y**2-y_friction_energy)*2/self.mass)**0.5*(self.velocity.y/abs(self.velocity.y))
+        
+    def ball_collide(self,system,ball):
+        ## move outside each other
+        dis = ((self.x-ball.x)**2+(self.y-ball.y)**2)**0.5
+        overlap = dis-ball.r-self.r
+
+        self.x -= overlap*(self.x-ball.x)/dis*(self.mass/(self.mass+ball.mass))
+        ball.x -= overlap*(ball.x-self.x)/dis*(ball.mass/(self.mass+ball.mass))
+
+        self.y -= overlap*(self.y-ball.y)/dis*(self.mass/(self.mass+ball.mass))
+        ball.y -= overlap*(ball.y-self.y)/dis*(ball.mass/(self.mass+ball.mass))
+
+        ## calculate velocities
+        
+        alpha = math.atan2(self.y-ball.y,self.x-ball.x)
+##        rotation_matrix = np.array([[1,0],[0,1]])
+        rotation_matrix = np.array([[math.cos(alpha),-math.sin(alpha)],
+                                    [math.sin(alpha),math.cos(alpha)]])
+        rotation_matrix_inv = np.linalg.inv(rotation_matrix)
+        
+        Ua = np.matmul(rotation_matrix,self.velocity.matrix())
+        Ub = np.matmul(rotation_matrix,ball.velocity.matrix())
+        Ma = self.mass
+        Mb = ball.mass
+        e = self.restitution*ball.restitution
+
+        symultanious_mat = np.array([[-1,0,1,0],
+                                     [0,-1,0,1],
+                                     [Ma,0,Mb,0],
+                                     [0,Ma,0,Mb]])
+        symultanious_vec = np.array([[e*(Ua[0][0]-Ub[0][0])],
+                                    [Ua[1][0]-Ub[1][0]],
+                                    [Ma*Ua[0][0]-Mb*Ub[0][0]],
+                                    [Ma*Ua[1][0]-Mb*Ub[1][0]]])
+        Out_Vector = np.matmul(np.linalg.inv(symultanious_mat),symultanious_vec)
+
+        Va = np.matmul(rotation_matrix_inv,np.array([[Out_Vector[0][0]],[Out_Vector[1][0]]]))
+        Vb = np.matmul(rotation_matrix_inv,np.array([[Out_Vector[2][0]],[Out_Vector[3][0]]]))
+##        print(self.velocity)
+##        print(rotation_matrix)
+##        print(Ua)
+##        print(,Ua,Ub,Va,Vb)
+        self.velocity.set_matrix(Va)
+        ball.velocity.set_matrix(Vb)
+
+        
         
     
     def vector_to_modarg(self):
@@ -125,13 +196,14 @@ class System:
         self.border = 5
         self.time_tracker = time.perf_counter()
 
-        self.speed_mul = 0.6
+        self.speed_mul = 1
 
-        self.wall_restitution = 0.5
-        self.wall_friction = 0.8
+        self.wall_restitution = 1#0.5
+        self.wall_friction = 0#0.3
 
-        self.circles = [Ball(self) for a in range(50)]
+        self.circles = [Ball(self) for a in range(20)]
 ##        self.circles = [Ball(self,0.5,7.5,1,0,0.5)]
+##        self.circles = [Ball(self,2,4-1/(2**0.5),1,0,0.5),Ball(self,6,4,-2,0,0.5)]
 
     def draw(self,screen):
         surf = pygame.Surface((self.width_pixels,self.height_pixels))
